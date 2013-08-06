9b2a30280034bb69948b4034a096381ad595feca
jbkkd <omerkorner@gmail.com>
2013-08-06 17:11:46 +0300

    Inital Commit

---
+++ Break/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Break
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.finished = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Location = new System.Drawing.Point(26, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(228, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Take a break!";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // finished
+            // 
+            this.finished.Location = new System.Drawing.Point(92, 158);
+            this.finished.Name = "finished";
+            this.finished.Size = new System.Drawing.Size(75, 23);
+            this.finished.TabIndex = 1;
+            this.finished.Text = "Finished";
+            this.finished.UseVisualStyleBackColor = true;
+            this.finished.Click += new System.EventHandler(this.finished_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            this.notifyIcon1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseMove);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Next Break In:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(111, 127);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "30";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.finished);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Break Time";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button finished;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Break/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Break
+{
+    public partial class Form1 : Form
+    {
+        DateTime Ticked;
+        int interval;
+        System.Timers.Timer timer = new System.Timers.Timer();
+        public Form1()
+        {
+            InitializeComponent();
+            timer.Stop();
+            Ticked = DateTime.Now;
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(timer1_Tick);
+        }
+
+        private void finished_Click(object sender, EventArgs e)
+        {
+            Ticked = DateTime.Now;
+            interval = 60000 * Convert.ToInt32(textBox1.Text);
+            timer.Interval = interval;
+            timer.Start();
+            this.Visible = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer.Stop();
+            this.Invoke(new Action(() => this.Show()));
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void notifyIcon1_MouseMove(object sender, MouseEventArgs e)
+        {
+            notifyIcon1.Text = "Time to next break: " + (Convert.ToInt32(textBox1.Text) - DateTime.Now.Subtract(Ticked).Minutes).ToString() + " Minutes";
+        }
+    }
+}
